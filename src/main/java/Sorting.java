cf706b666ffeb9b4638c6385a6fc98e4f89db979
sam <samosborn88@gmail.com>
2016-07-11 20:05:38 -0500

    add merge sort with tests and exceptions

--- src/main/java/Sorting.java
+++ src/main/java/Sorting.java
@@ -1,6 +1,7 @@
 
 
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -39,5 +40,65 @@ public class Sorting {
         }
 
         return array;
+    }
+
+    /*
+        input array of sortable items?
+         ints for now
+         cut array in half recursively until each array is size 1
+         merge the two subarrays by grabbing the next from the two arrays that fits sort
+         for sorting ints ascending, grab lower of two ints
+     */
+    public static List<Integer> mergeSort(List<Integer> array) {
+        if(array == null) {
+            throw new IllegalArgumentException("invalid input array to merge sort: input array is null");
+        }
+
+        if(array.isEmpty()) {
+            throw new IllegalArgumentException("invalid input array to merge sort: empty array");
+        }
+
+        return mergeSort(array, 0, array.size() - 1);
+    }
+
+    public static List<Integer> mergeSort(List<Integer> array, int start, int end) throws IllegalArgumentException {
+
+        if(start < end) {
+            int middle = Math.round((start + end) / 2);
+            mergeSort(array, start, middle);
+            mergeSort(array, middle + 1, end);
+            merge(array,start,middle,end);
+        }
+
+        return  array;
+    }
+
+    private static void merge(List<Integer> array, int start, int middle, int end) {
+
+        // array.subList(inclusive,exclusive) so middle will be start of right array
+        middle++;
+        // array.subList(start,end) will throw exception if(start < 0 || end > size)
+        end++;
+        LinkedList<Integer> leftArray = new LinkedList<Integer>(array.subList(start, middle));
+        LinkedList<Integer> rightArray = new LinkedList<Integer>(array.subList(middle, end));
+
+        int index = start;
+        while(! (leftArray.isEmpty() && rightArray.isEmpty())){
+
+            int value;
+            if(leftArray.isEmpty()) {
+                value = rightArray.remove();
+            } else if(rightArray.isEmpty()) {
+                value = leftArray.remove();
+            } else {
+                if(leftArray.getFirst() < rightArray.getFirst()) {
+                    value = leftArray.remove();
+                } else {
+                    value = rightArray.remove();
+                }
+            }
+            array.set(index, value);
+            index++;
+        }
     }
 }
--- src/test/java/SortingTest.java
+++ src/test/java/SortingTest.java
@@ -22,6 +22,12 @@ public class SortingTest {
 
         return array;
     }
+    
+    
+    private List<Integer> sort(List<Integer> list) {
+        return  Sorting.mergeSort(list);
+    }
+    
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();
@@ -29,21 +35,21 @@ public class SortingTest {
     @Test
     public void test1() {
         List<Integer> expected = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
-        List<Integer> actual = Sorting.bubbleSort(Arrays.asList(10,9,6,1,7,2,3,5,4,8));
+        List<Integer> actual = sort(Arrays.asList(10,9,6,1,7,2,3,5,4,8));
         Assert.assertEquals(expected,actual);
     }
 
     @Test
     public void test2() {
         List<Integer> expected = Arrays.asList(0, 14, 19, 27, 48, 56, 58, 60, 65, 71);
-        List<Integer> actual = Sorting.bubbleSort(Arrays.asList(58, 19, 60, 14, 0, 27, 48, 56, 71, 65));
+        List<Integer> actual = sort(Arrays.asList(58, 19, 60, 14, 0, 27, 48, 56, 71, 65));
         Assert.assertEquals(expected,actual);
     }
 
     @Test
     public void test3() {
         List<Integer> expected = Arrays.asList(16, 24, 30, 38, 58, 68, 68, 82, 88, 98);
-        List<Integer> actual = Sorting.bubbleSort(Arrays.asList(82, 30, 24, 16, 38, 58, 88, 68, 68, 98));
+        List<Integer> actual = sort(Arrays.asList(82, 30, 24, 16, 38, 58, 88, 68, 68, 98));
         Assert.assertEquals(expected,actual);
     }
 
@@ -51,7 +57,7 @@ public class SortingTest {
     public void emptyArrayTest() {
         thrown.expect(IllegalArgumentException.class);
         List<Integer> expected = new ArrayList<Integer>();
-        Assert.assertEquals(expected, Sorting.bubbleSort(new ArrayList<Integer>()));
+        Assert.assertEquals(expected, sort(new ArrayList<Integer>()));
         thrown.expectMessage("invalid input array to bubble sort");
     }
 
@@ -60,13 +66,13 @@ public class SortingTest {
     public void nullArray() {
         thrown.expect(IllegalArgumentException.class);
         List<Integer> expected = new ArrayList<Integer>();
-        Assert.assertEquals(expected, Sorting.bubbleSort(null));
+        Assert.assertEquals(expected, sort(null));
     }
     /*
         @Test
         public void test() {
             List<Integer> expected = randomArray();
-            List<Integer> actual = Sorting.bubbleSort(Arrays.asList(10,9,6,1,7,2,3,5,4,8));
+            List<Integer> actual = sort(Arrays.asList(10,9,6,1,7,2,3,5,4,8));
             Assert.assertEquals(expected,actual);
         }
      */

1c6fdcea3434f68b4896f29ac800030d7a896150
sam <samosborn88@gmail.com>
2016-10-31 23:39:38 -0500

    adds a few tests

--- src/test/java/BuildingsCatchingWaterTest.java
+++ src/test/java/BuildingsCatchingWaterTest.java
@@ -36,6 +36,30 @@ import org.junit.Test;
  * input: [3, 1, 2, 1, 2, 4, 1]
  * catch: [0, 2, 1, 2, 1, 0, 0]
  * total: 6
+ *
+ * Test Case #4
+ * 5 |             __
+ * 4 |          __|  |__
+ * 3 |       __|  |  |  |__
+ * 2 |    __|  |  |  |  |  |__
+ * 1 |   |  |  |  |  |  |  |  |
+ * ===================================================================
+ * input: [1, 2, 3, 4, 3, 2, 1]
+ * catch: [0, 0, 0, 0, 0, 0, 0]
+ * total: 0
+ *
+ * Test Case #5
+ *                                   __
+ * 5 |                   __         |  |
+ * 4 |    __            |  |      __|  |
+ * 3 |   |  |   __    __|  |   __|  |  |
+ * 2 |   |  |__|  |__|  |  |__|  |  |  |
+ * 1 |   |  |  |  |  |  |  |  |  |  |  |
+ * ===================================================================
+ * input: [3, 1, 2, 1, 2, 4, 1, 2, 3, 5]
+ * catch: [0, 2, 1, 2, 1, 0, 3, 2, 1, 0]
+ * total: 12
+ *
  */
 public class BuildingsCatchingWaterTest {
 
@@ -57,6 +81,20 @@ public class BuildingsCatchingWaterTest {
     public void testCase3() {
         int[] buildings = new int[]{3, 1, 2, 1, 2, 4, 1};
         int   expectedWaterCaught = 6;
+        Assert.assertEquals(expectedWaterCaught, BuildingsCatchingWater.compute(buildings));
+    }
+
+    @Test
+    public void testCase4() {
+        int[] buildings = new int[]{1, 2, 3, 4, 3, 2, 1};
+        int   expectedWaterCaught = 0;
+        Assert.assertEquals(expectedWaterCaught, BuildingsCatchingWater.compute(buildings));
+    }
+
+    @Test
+    public void testCase5() {
+        int[] buildings = new int[]{3, 1, 2, 1, 2, 4, 1, 2, 3, 5};
+        int   expectedWaterCaught = 12;
         Assert.assertEquals(expectedWaterCaught, BuildingsCatchingWater.compute(buildings));
     }
 }
